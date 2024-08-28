9b32d8e8b85df3920c3c582ee3e9e5af9afe5eca
Mkm43 <mehtabanwar717@gmail.com>
2024-08-28 07:11:56 -0700

    Update config.js

--- config.js
+++ config.js
@@ -6,20 +6,20 @@ global.audio = "";
 global.video = "";
 global.port = process.env.PORT;
 global.appUrl = process.env.APP_URL || "";
-global.email = "Hechem254@gmail.com";
-global.location = "Nairobi, Africa.";
+global.email = "coming soon";
+global.location = "pakistan, gilgit.";
 global.mongodb = process.env.MONGODB_URI || "";
 global.allowJids = process.env.ALLOW_JID || "null";
 global.blockJids = process.env.BLOCK_JID || "null";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
 global.timezone = process.env.TZ || process.env.TIME_ZONE || "Afica/Nairobi";
-global.github = process.env.GITHUB || "https://github.com/Hammy254/VENOM_MD";
-global.gurl = process.env.GURL || "https://chat.whatsapp.com/LqYfmGAmfbnHhtSWSsXP1v";
-global.website = process.env.GURL || "https://chat.whatsapp.com/LqYfmGAmfbnHhtSWSsXP1v";
-global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/f713683bc977188572040.jpg";
-global.devs = "254737991043";
-global.sudo = process.env.SUDO || "254737991043";
-global.owner = process.env.OWNER_NUMBER || "254737991043";
+global.github = process.env.GITHUB || "https://github.com/Mkm43/VENOM";
+global.gurl = process.env.GURL || "https://api.whatsapp.com/send?phone=+923157843661";
+global.website = process.env.GURL || "https://api.whatsapp.com/send?phone=+923157843661";
+global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/28dd0fc5e1abdfcdd0586.jpg";
+global.devs = "923157843661";
+global.sudo = process.env.SUDO || "923157843661";
+global.owner = process.env.OWNER_NUMBER || "923157843661";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -32,7 +32,7 @@ global.waPresence = process.env.WAPRESENCE || "online";
 global.readcmds = process.env.READ_COMMAND || "false";
 global.readmessage = process.env.READ_MESSAGE || "false";
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
-global.read_status = process.env.AUTO_READ_STATUS || "true";
+global.read_status = process.env.AUTO_READ_STATUS || "false";
 global.save_status = process.env.AUTO_SAVE_STATUS || "false";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "";
 global.read_status_from = process.env.READ_STATUS_FROM || "";
@@ -49,11 +49,11 @@ module.exports = {
   BRANCH: process.env.BRANCH || "main",
   VERSION: process.env.VERSION || "1.0.0",
   caption: process.env.CAPTION || "𝐆𝐄𝐍𝐄𝐑𝐀𝐓𝐄𝐃 𝐁𝐘 𝐕𝐄𝐍𝐎𝐌_𝐌𝐃",
-  author: process.env.PACK_AUTHER || "〄༒𝐇𝐀𝐌𝐌𝐘➳ᴹᴿ᭄\n\n+254 737991043",
+  author: process.env.PACK_AUTHER || "mehtab",
   packname: process.env.PACK_NAME || "𝐒𝐓𝐈𝐂𝐊𝐄𝐑 𝐁𝐘",
-  botname: process.env.BOT_NAME || "𝐕𝐄𝐍𝐎𝐌_𝐌𝐃",
-  ownername: process.env.OWNER_NAME || "⟠ 〄༒𝐇𝐀𝐌𝐌𝐘➳ᴹᴿ᭄",
-  errorChat: process.env.ERROR_CHAT || "254737991043",
+  botname: process.env.BOT_NAME || "mehtab",
+  ownername: process.env.OWNER_NAME || "⟠ 〄༒mehtabᴹᴿ᭄",
+  errorChat: process.env.ERROR_CHAT || "923157843661",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
   OPENAI_API_KEY: process.env.OPENAI_API_KEY || "",
